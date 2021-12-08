f5613324a4e7c42beca7639895703174c591fdf5
Lucy3020 <67004463+Lucy3020@users.noreply.github.com>
2021-12-08 12:39:04 +0700

    Create libraryFragment.java
    libraryFragment

---
+++ MusicLovers/app/src/main/java/com/example/musiclovers/fragments/libraryFragment.java
@@ -0,0 +1,122 @@
+package com.example.musiclovers.fragments;
+
+import android.os.Bundle;
+import android.transition.TransitionInflater;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.musiclovers.MainActivity;
+import com.example.musiclovers.PlaceHolder;
+import com.example.musiclovers.R;
+import com.example.musiclovers.ViewModel;
+import com.example.musiclovers.models.songItem;
+import com.example.musiclovers.listAdapter.songsListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * DONE
+ */
+public class libraryFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private songsListAdapter mAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TransitionInflater inflater = TransitionInflater.from(requireContext());
+        setEnterTransition(inflater.inflateTransition(R.transition.slide_right));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_library, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //playlist
+        ConstraintLayout playlist = view.findViewById(R.id.fragment_library_Playlist);
+        playlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((FragmentActivity) view.getContext()).getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, new playlistsFragment())
+                        .addToBackStack(null)
+                        .setReorderingAllowed(true)
+                        .commit();
+            }
+        });
+
+        //artist
+        ConstraintLayout artist = view.findViewById(R.id.fragment_library_Artist);
+        artist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((FragmentActivity) view.getContext()).getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, new artistsFragment())
+                        .addToBackStack(null)
+                        .setReorderingAllowed(true)
+                        .commit();
+            }
+        });
+
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.fragment_library_AllSongs);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        String base_Url = "http://10.0.2.2:3000/";
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(base_Url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        PlaceHolder placeHolder = retrofit.create(PlaceHolder.class);
+        Call<List<songItem>> call = placeHolder.getSongs();
+        call.enqueue(new Callback<List<songItem>>() {
+            @Override
+            public void onResponse(Call<List<songItem>> call, Response<List<songItem>> response) {
+                if(!response.isSuccessful()) {
+                    Toast.makeText(getContext(), "code: "+ response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ArrayList<songItem> songItems = (ArrayList<songItem>) response.body();
+
+                mAdapter = new songsListAdapter(
+                        R.layout.song_format,
+                        R.id.song_format_SongName,
+                        R.id.song_format_ArtistName,
+                        R.id.song_format_SongImg,
+                        songItems,
+                        getContext());
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<songItem>> call, Throwable t) {
+                Toast.makeText(getContext(), "error", Toast.LENGTH_LONG);
+            }
+        });
+
+    }
+}
